b78b9bd1e213f82ccc24ea35a43c8d54e9755a3d
Denise <dlaugins@gmail.com>
2019-03-17 18:15:42 -0500

    Updates

--- precache-manifest.97131dad48d3321e9851db2ff4aba8e5.js
+++ precache-manifest.97131dad48d3321e9851db2ff4aba8e5.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/reactclickyhw/static/js/runtime~main.77a628c3.js"
   },
   {
-    "revision": "a722c5cefd10b9a3d8f3",
-    "url": "/reactclickyhw/static/js/main.a722c5ce.chunk.js"
+    "revision": "883561f31685ec92a6e0",
+    "url": "/reactclickyhw/static/js/main.883561f3.chunk.js"
   },
   {
     "revision": "f05a1df3217379e807f2",
     "url": "/reactclickyhw/static/js/2.f05a1df3.chunk.js"
   },
   {
-    "revision": "a722c5cefd10b9a3d8f3",
+    "revision": "883561f31685ec92a6e0",
     "url": "/reactclickyhw/static/css/main.1e822265.chunk.css"
   },
   {
-    "revision": "f6c0af4e1ff9bfa1f81eca1c80436ab1",
+    "revision": "4d909c17ecde8c129561b3388f0770b4",
     "url": "/reactclickyhw/index.html"
   }
 ];
